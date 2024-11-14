2410561157bd6a7ce331a195805a36b8d47797db
Thomas Fuzesi <thomas.fuzesi@countingup.com>
2024-11-14 16:10:54 +0000

    logging in and persisting favs

--- app/api/works/[workId]/fav+api.ts
+++ app/api/works/[workId]/fav+api.ts
@@ -2,10 +2,10 @@ import { Database } from "@/data/api/database";
 
 export async function GET(
   request: Request,
-  { workId }: Record<string, string>,
+  { workId }: Record<string, string>
 ) {
   // read the favorites status from our database
-  const database = new Database();
+  const database = new Database(request.headers.get("authToken"));
   const favStatus = await database.getFavoriteStatus(workId);
   // make a json response
   return Response.json(favStatus);
@@ -13,13 +13,13 @@ export async function GET(
 
 export async function POST(
   request: Request,
-  { workId }: Record<string, string>,
+  { workId }: Record<string, string>
 ) {
   // read the body for the payload
   const body = await request.json();
   const status = body.status;
   // write the updated status to our database
-  const database = new Database();
+  const database = new Database(request.headers.get("authToken"));
   await database.setFavoriteStatus(workId, status);
   // make a json response
   return Response.json(status);
