7129ec5489cd7716f117e5dacdd2451b22b35e20
Thomas Fuzesi <thomas.fuzesi@countingup.com>
2024-11-14 16:40:40 +0000

    outside the tabs customisation

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -3,6 +3,7 @@ import { View } from "react-native";
 import { TabList, Tabs, TabSlot, TabTrigger } from "expo-router/build/ui";
 import { TabButton } from "@/components/TabButton";
 import classNames from "classnames";
+import { Image } from "expo-image";
 
 const tabs = (
   <TabList
@@ -10,7 +11,8 @@ const tabs = (
       "py-3 sm:py-6",
       "px-6 sm:px-8",
       "mx-2 sm:mx-0",
-      "bg-white",
+      "sm:justify-end sm:gap-x-4 sm:shadow-sm",
+      "bg-white"
     )}
   >
     <TabTrigger name="index" href="/" asChild>
@@ -31,12 +33,23 @@ const tabs = (
 export default function TabLayout() {
   return (
     <View className="flex-1">
-      <Tabs className="flex-1">
+      <Tabs className="flex-1 sm:flex-col-reverse">
         <View className="flex-1">
           <TabSlot />
         </View>
         {tabs}
       </Tabs>
+      <View
+        className={classNames(
+          "hidden sm:inline",
+          "absolute left-6 top-5 h-10 w-52",
+        )}
+      >
+        <Image
+          source={require("@/assets/images/logo.svg")}
+          className="w-full h-full"
+        />
+      </View>
     </View>
   );
 }
