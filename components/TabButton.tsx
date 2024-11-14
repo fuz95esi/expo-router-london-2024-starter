d499b4e5c58277c423bd602b785fcd99bc48ff57
Thomas Fuzesi <thomas.fuzesi@countingup.com>
2024-11-14 16:45:23 +0000

    customisation of TabButton

--- components/TabButton.tsx
+++ components/TabButton.tsx
@@ -12,17 +12,27 @@ export type TabButtonProps = TabTriggerSlotProps & {
 
 export const TabButton = forwardRef(function TabButtonComponent(
   { icon, children, isFocused, ...props }: TabButtonProps,
-  ref: Ref<View>,
+  ref: Ref<View>
 ) {
   return (
-    <Pressable ref={ref} {...props}>
+    <Pressable
+      ref={ref}
+      {...props}
+      className={isFocused ? " sm:border-b-tint sm:border-b-2" : ""}
+    >
       <View className="justify-between items-center gap-y-1 px-2 flex-col">
         <MaterialIcons
           color={isFocused ? colors.tint : colors.black}
+          className="sm:hidden"
           name={icon}
           size={24}
         />
-        <Text className={"text-md" + (isFocused ? " color-tint" : "")}>
+        <Text
+          className={
+            "text-sm sm:text-lg" +
+            (isFocused ? " color-tint sm:color-black" : "")
+          }
+        >
           {children}
         </Text>
       </View>
