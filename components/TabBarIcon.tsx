23c293a18b172aba6dfdd73f50ed160037fa1701
Thomas Fuzesi <thomas.fuzesi@countingup.com>
2024-11-14 14:38:10 +0000

    bug: move ts-ignore

--- components/TabBarIcon.tsx
+++ components/TabBarIcon.tsx
@@ -13,8 +13,8 @@ export function TabBarIcon(props: {
   const IconComponent =
     props.type === "MaterialIcons" ? MaterialIcons : FontAwesome;
   const myColor = props.color || "black";
-  // @ts-ignore
   return (
+    // @ts-ignore
     <IconComponent
       size={28}
       style={{ marginBottom: -3 }}
