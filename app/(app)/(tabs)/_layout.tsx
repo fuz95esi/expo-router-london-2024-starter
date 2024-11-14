adc3011235d0809b98b30289e98f99220e7977e9
Thomas Fuzesi <thomas.fuzesi@countingup.com>
2024-11-14 16:24:40 +0000

    custom tab navigator ui

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,57 +1,42 @@
 import React from "react";
-import { Tabs } from "expo-router";
-import customColors from "@/constants/colors";
-import colors from "@/constants/colors";
-import { TabBarIcon } from "@/components/TabBarIcon";
+import { View } from "react-native";
+import { TabList, Tabs, TabSlot, TabTrigger } from "expo-router/build/ui";
+import { TabButton } from "@/components/TabButton";
+import classNames from "classnames";
+
+const tabs = (
+  <TabList
+    className={classNames(
+      "py-3 sm:py-6",
+      "px-6 sm:px-8",
+      "mx-2 sm:mx-0",
+      "bg-white",
+    )}
+  >
+    <TabTrigger name="index" href="/" asChild>
+      <TabButton icon="museum">Home</TabButton>
+    </TabTrigger>
+    <TabTrigger name="exhibits" asChild href="/exhibits">
+      <TabButton icon="palette">Exhibits</TabButton>
+    </TabTrigger>
+    <TabTrigger name="visit" asChild href="/visit">
+      <TabButton icon="map">Visit</TabButton>
+    </TabTrigger>
+    <TabTrigger name="profile" asChild href="/profile">
+      <TabButton icon="person">Profile</TabButton>
+    </TabTrigger>
+  </TabList>
+);
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        lazy: false,
-        sceneStyle: {
-          backgroundColor: colors.white,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarActiveTintColor: customColors.tint,
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon type="MaterialIcons" name="museum" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="exhibits"
-        options={{
-          title: "Exhibits",
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon type="MaterialIcons" name="palette" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="visit"
-        options={{
-          title: "Visit",
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon type="MaterialIcons" name="map" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon type="MaterialIcons" name="person" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <View className="flex-1">
+      <Tabs className="flex-1">
+        <View className="flex-1">
+          <TabSlot />
+        </View>
+        {tabs}
+      </Tabs>
+    </View>
   );
 }
